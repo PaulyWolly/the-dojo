f911a22672915714ebf7aa6e32bdce2894440057
Paul Welby <pwelby@gmail.com>
2022-02-03 08:21:51 -0800

    Adjusted styles

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import { useCollection } from '../../hooks/useCollection'
+import ProjectFilter from './ProjectFilter'
 
 // components
 import ProjectList from '../../components/ProjectList'
@@ -15,6 +16,7 @@ export default function Dashboard() {
     <div>
       <h2 className="page-title">Dashboard</h2>
       {error && <p className="error">{error}</p>}
+      {documents && <ProjectFilter />}
       {documents && <ProjectList projects={documents} />}
     </div>
   )
